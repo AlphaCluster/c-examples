85435c6a0a943103a814dbc8c5d1acae2db2c060
Nicholas Omann <AlphaCluster@linux.com>
2023-08-13 08:29:23 -0500

    finish basic app

--- math-example/main.c
+++ math-example/main.c
@@ -5,6 +5,33 @@
 
 const size_t BUFFER_SIZE = 512;
 
+enum OpCode {Add, Sub, Mult, Div, None};
+
+enum OpCode parse_operator(char operation)
+{
+    enum OpCode result;
+    switch (operation)
+    {
+        case '+':
+            result = Add;
+            break;
+        case '-':
+            result = Sub;
+            break;
+        case '*':
+            result = Mult;
+            break;
+        case '/':
+            result = Div;
+            break;
+        default:
+            result = None;
+            break;
+    }
+
+    return result;
+}
+
 bool parse_line(long *first, long *second, char *operation, const char *buffer)
 {
     char localBuffer[BUFFER_SIZE];
@@ -19,10 +46,18 @@ bool parse_line(long *first, long *second, char *operation, const char *buffer)
             *operation = buffer[index];
             // Save first number as this is the middle
             *first = strtol(localBuffer, NULL, 10);
-            localBuffer[0] = 0;
+
+            // Since we found an operator we can jump to the next spot
+            // this makes sure the load that will take place later doesn't
+            // end up with an operator.
+            index++;
+            if ((index < BUFFER_SIZE) && (index < strlen(buffer)))
+                memset(localBuffer, 0, BUFFER_SIZE);
+            else
+                return false;
         }
 
-        if (buffer[index] == '\0')
+        if (buffer[index] == '\n')
         {
             *second = strtol(localBuffer, NULL, 10);
             return true;
@@ -34,14 +69,36 @@ bool parse_line(long *first, long *second, char *operation, const char *buffer)
     return false;
 }
 
+long calculate_result(long first, long second, enum OpCode operation)
+{
+    long result = 0;
+    switch (operation)
+    {
+        case Add:
+            result = first + second;
+            break;
+        case Sub:
+            result = first - second;
+            break;
+        case Mult:
+            result = first * second;
+            break;
+        case Div:
+            result = first / second;
+            break;
+    }
+
+    return result;
+}
+
 int main() {
     char *line = NULL;
     size_t length = 0;
-    ssize_t status = 0;
+    ssize_t rStatus = 0;
 
     printf("Enter a equation:\n");
 
-    status = getline(&line, &length, stdin);
+    rStatus = getline(&line, &length, stdin);
     // Move to local buffer so we can make sure the dynamic memory is freed.
     char buffer[BUFFER_SIZE];
     strcpy(buffer, line);
@@ -51,8 +108,10 @@ int main() {
     long first;
     long second;
     char operand;
-    bool result = parse_line(&first, &second, &operand, buffer);
+    bool status = parse_line(&first, &second, &operand, buffer);
 
-    printf("Read: %ld %c %ld", first, operand, second);
+    long result = calculate_result(first, second, parse_operator(operand));
+
+    printf("Read: %ld %c %ld = %ld", first, operand, second, result);
     return 0;
 }
